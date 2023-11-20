63295e4d25a6056c488645171467dbff926771ea
Prokuma <prokuma@prokuma.dev>
2023-11-20 18:01:27 +0900

    [Update] Change SpMM and MM order

--- src/layer.c
+++ src/layer.c
@@ -103,69 +103,131 @@ void gcn_propagation(GCNNetwork *network) {
     #endif
     int layer_cnt = 0;
     while (p != NULL) {
-        out_size = network->graph->matrix.row_size * p->hidden_layer.col_size;
-        r_spmm.row_size = network->graph->matrix.row_size;
-        r_spmm.col_size = p->latent_vectors.col_size;
-        r_mm.row_size = p->latent_vectors.row_size;
-        r_mm.col_size = p->hidden_layer.col_size;
-        allocDenseMatrix(&r_spmm); allocDenseMatrix(&r_mm);
-        
-        #if defined(EMAX6) || defined(EMAX7)
-            imax_dense_format_init_from_sparse(&h, &network->graph->imax_matrix, p->latent_vectors.col_size, 8);
-            imax_dense_format_init(&imax_r_spmm, h.row_size, h.col_size, h.row_padded_size, h.col_padded_size, h.blk_row_size, h.blk_col_size);
-            imax_dense_format_init(&w, imax_r_spmm.col_size, p->hidden_layer.col_size, imax_r_spmm.col_padded_size, p->hidden_layer.col_size + imax_r_spmm.blk_col_size - (p->hidden_layer.col_size%imax_r_spmm.blk_col_size), MM_H, imax_r_spmm.blk_col_size);
-            imax_dense_format_init(&imax_r_mm, imax_r_spmm.row_size, w.col_size, imax_r_spmm.row_padded_size, w.col_padded_size, imax_r_spmm.blk_row_size, w.blk_col_size);
-            imax_gcn_allocation(&network->graph->imax_matrix, &h, &imax_r_spmm, &w, &imax_r_mm);
-            convert_imax_dense_format(&h, &(p->latent_vectors));
-            convert_imax_dense_format(&w, &(p->hidden_layer));
-        #endif
-
-        printf("Layer %d: SpMM\n", ++layer_cnt);
-        HiddenLayer t;
-        timespec_get(&t1, TIME_UTC);
-        #if defined(EMAX6) || defined(EMAX7)
-            spmm(&imax_r_spmm, &(network->graph->imax_matrix), &h);
-        #else
-            spmm(&r_spmm, &(network->graph->matrix), &(p->latent_vectors));
-        #endif
-        timespec_get(&t2, TIME_UTC);
-        spmm_time += cal_time(&t2, &t1);
-
-        #if defined(EMAX6) || defined(EMAX7)
-            convert_dense_format(&r_spmm, &imax_r_spmm);
-        #elif defined(USE_CUDA)
-            sendDenseMatrixToCPU(&r_spmm);
-        #endif
-        print_weight(&r_spmm);
-
-        printf("Layer %d: MM\n", layer_cnt);
-        timespec_get(&t1, TIME_UTC);
-        #if defined(EMAX6) || defined(EMAX7)
-            mm(&imax_r_mm, &imax_r_spmm, &w);
-        #else
-            mm(&r_mm, &r_spmm, &(p->hidden_layer));
-        #endif
-        timespec_get(&t2, TIME_UTC);
-        mm_time += cal_time(&t2, &t1);
-
-        #if defined(EMAX6) || defined(EMAX7)
-            convert_dense_format(&r_mm, &imax_r_mm);
-        #elif defined(USE_CUDA)
-            sendDenseMatrixToCPU(&r_mm);
-        #endif
-        print_weight(&r_mm);
-
         if (p->next == NULL) last_weight = &(network->layers->result_layer);
         else last_weight = &(p->next->latent_vectors);
+        if (p->latent_vectors.col_size > p->hidden_layer.col_size) {
+            out_size = network->graph->matrix.row_size * p->hidden_layer.col_size;
+            r_spmm.row_size = network->graph->matrix.row_size;
+            r_spmm.col_size = p->hidden_layer.col_size;
+            r_mm.row_size = p->latent_vectors.row_size;
+            r_mm.col_size = p->hidden_layer.col_size;
+            allocDenseMatrix(&r_spmm); allocDenseMatrix(&r_mm);
+            
+            #if defined(EMAX6) || defined(EMAX7)
+                imax_dense_format_init_from_sparse(&h, &network->graph->imax_matrix, p->latent_vectors.col_size, 8);
+                imax_dense_format_init(&w, h.col_size, p->hidden_layer.col_size, h.col_padded_size, p->hidden_layer.col_size + h.blk_col_size - (p->hidden_layer.col_size%h.blk_col_size), MM_H, h.blk_col_size);
+                imax_dense_format_init(&imax_r_mm, h.row_size, w.col_size, h.row_padded_size, w.col_padded_size, h.blk_row_size, w.blk_col_size);
+                imax_dense_format_init(&imax_r_spmm, imax_r_mm.row_size, imax_r_mm.col_size, imax_r_mm.row_padded_size, imax_r_mm.col_padded_size, imax_r_mm.blk_row_size, imax_r_mm.blk_col_size);
+                imax_gcn_allocation(&network->graph->imax_matrix, &h, &imax_r_spmm, &w, &imax_r_mm);
+                convert_imax_dense_format(&h, &(p->latent_vectors));
+                convert_imax_dense_format(&w, &(p->hidden_layer));
+            #endif
 
-        printf("Layer %d: ReLU\n", layer_cnt);
-        timespec_get(&t1, TIME_UTC);
-        relu(last_weight, &r_mm);
-        timespec_get(&t2, TIME_UTC);
-        #ifdef USE_CUDA
-            if (p->next == NULL) sendDenseMatrixToCPU(last_weight);
-        #endif
-        relu_time += cal_time(&t2, &t1);
+            HiddenLayer t;
+            printf("Layer %d: MM\n", ++layer_cnt);
+            timespec_get(&t1, TIME_UTC);
+            #if defined(EMAX6) || defined(EMAX7)
+                mm(&imax_r_mm, &h, &w);
+            #else
+                mm(&r_mm, &(p->latent_vectors), &(p->hidden_layer));
+            #endif
+            timespec_get(&t2, TIME_UTC);
+            mm_time += cal_time(&t2, &t1);
+
+            #if defined(EMAX6) || defined(EMAX7)
+                convert_dense_format(&r_mm, &imax_r_mm);
+            #elif defined(USE_CUDA)
+                sendDenseMatrixToCPU(&r_mm);
+            #endif
+            print_weight(&r_mm);
+
+            printf("Layer %d: SpMM\n", layer_cnt);
+            timespec_get(&t1, TIME_UTC);
+            #if defined(EMAX6) || defined(EMAX7)
+                spmm(&imax_r_spmm, &(network->graph->imax_matrix), &imax_r_mm);
+            #else
+                spmm(&r_spmm, &(network->graph->matrix), &r_mm);
+            #endif
+            timespec_get(&t2, TIME_UTC);
+            spmm_time += cal_time(&t2, &t1);
+
+            #if defined(EMAX6) || defined(EMAX7)
+                convert_dense_format(&r_spmm, &imax_r_spmm);
+            #elif defined(USE_CUDA)
+                sendDenseMatrixToCPU(&r_spmm);
+            #endif
+            print_weight(&r_spmm);
+
+            printf("Layer %d: ReLU\n", layer_cnt);
+            timespec_get(&t1, TIME_UTC);
+            relu(last_weight, &r_spmm);
+            timespec_get(&t2, TIME_UTC);
+            #ifdef USE_CUDA
+                if (p->next == NULL) sendDenseMatrixToCPU(last_weight);
+            #endif
+            relu_time += cal_time(&t2, &t1);
+        } else {
+            out_size = network->graph->matrix.row_size * p->hidden_layer.col_size;
+            r_spmm.row_size = network->graph->matrix.row_size;
+            r_spmm.col_size = p->latent_vectors.col_size;
+            r_mm.row_size = p->latent_vectors.row_size;
+            r_mm.col_size = p->hidden_layer.col_size;
+            allocDenseMatrix(&r_spmm); allocDenseMatrix(&r_mm);
+            
+            #if defined(EMAX6) || defined(EMAX7)
+                imax_dense_format_init_from_sparse(&h, &network->graph->imax_matrix, p->latent_vectors.col_size, 8);
+                imax_dense_format_init(&imax_r_spmm, h.row_size, h.col_size, h.row_padded_size, h.col_padded_size, h.blk_row_size, h.blk_col_size);
+                imax_dense_format_init(&w, imax_r_spmm.col_size, p->hidden_layer.col_size, imax_r_spmm.col_padded_size, p->hidden_layer.col_size + imax_r_spmm.blk_col_size - (p->hidden_layer.col_size%imax_r_spmm.blk_col_size), MM_H, imax_r_spmm.blk_col_size);
+                imax_dense_format_init(&imax_r_mm, imax_r_spmm.row_size, w.col_size, imax_r_spmm.row_padded_size, w.col_padded_size, imax_r_spmm.blk_row_size, w.blk_col_size);
+                imax_gcn_allocation(&network->graph->imax_matrix, &h, &imax_r_spmm, &w, &imax_r_mm);
+                convert_imax_dense_format(&h, &(p->latent_vectors));
+                convert_imax_dense_format(&w, &(p->hidden_layer));
+            #endif
+
+            printf("Layer %d: SpMM\n", ++layer_cnt);
+            HiddenLayer t;
+            timespec_get(&t1, TIME_UTC);
+            #if defined(EMAX6) || defined(EMAX7)
+                spmm(&imax_r_spmm, &(network->graph->imax_matrix), &h);
+            #else
+                spmm(&r_spmm, &(network->graph->matrix), &(p->latent_vectors));
+            #endif
+            timespec_get(&t2, TIME_UTC);
+            spmm_time += cal_time(&t2, &t1);
+
+            #if defined(EMAX6) || defined(EMAX7)
+                convert_dense_format(&r_spmm, &imax_r_spmm);
+            #elif defined(USE_CUDA)
+                sendDenseMatrixToCPU(&r_spmm);
+            #endif
+            print_weight(&r_spmm);
+
+            printf("Layer %d: MM\n", layer_cnt);
+            timespec_get(&t1, TIME_UTC);
+            #if defined(EMAX6) || defined(EMAX7)
+                mm(&imax_r_mm, &imax_r_spmm, &w);
+            #else
+                mm(&r_mm, &r_spmm, &(p->hidden_layer));
+            #endif
+            timespec_get(&t2, TIME_UTC);
+            mm_time += cal_time(&t2, &t1);
+
+            #if defined(EMAX6) || defined(EMAX7)
+                convert_dense_format(&r_mm, &imax_r_mm);
+            #elif defined(USE_CUDA)
+                sendDenseMatrixToCPU(&r_mm);
+            #endif
+            print_weight(&r_mm);
+
+            printf("Layer %d: ReLU\n", layer_cnt);
+            timespec_get(&t1, TIME_UTC);
+            relu(last_weight, &r_mm);
+            timespec_get(&t2, TIME_UTC);
+            #ifdef USE_CUDA
+                if (p->next == NULL) sendDenseMatrixToCPU(last_weight);
+            #endif
+            relu_time += cal_time(&t2, &t1);
+        }
 
         freeDenseMatrix(&r_spmm);freeDenseMatrix(&r_mm);
         p = p->next;
